1f2188efd4053657a5cd82cd48ee7cc0c0f9e909
changeaDream <1037197044@qq.com>
2021-10-22 20:41:41 +0800

    完成用户列表功能的开发

--- vue_shop/src/router/index.js
+++ vue_shop/src/router/index.js
@@ -2,14 +2,32 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes:[
-    { path: '/', redirect: '/Login' },
-    { path: '/Login', component: Login},
-    { path: '/Home', component: Home}
+  routes: [{
+      path: '/',
+      redirect: '/Login'
+    },
+    {
+      path: '/Login',
+      component: Login
+    },
+    {
+      path: '/Home',
+      component: Home,
+      redirect: '/Welcome',
+      children: [{
+        path: '/Welcome',
+        component: Welcome
+      }, {
+        path: '/users',
+        component: Users
+      }]
+    }
   ]
 })
 
