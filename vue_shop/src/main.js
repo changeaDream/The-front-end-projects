8d7c14293d2fdab308cfdad71844537133c39ccb
changeaDream <1037197044@qq.com>
2021-10-24 18:27:01 +0800

    完成了分类功能的开发

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -7,6 +7,8 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
+// 导入 vue-table-with-tree-grid 插件
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 //配置请求的根路径
@@ -20,6 +22,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
