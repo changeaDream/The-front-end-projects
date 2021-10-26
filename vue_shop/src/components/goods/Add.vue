42c1d0c1c1dab83ec6d0afe115c870d252ac373a
changeaDream <1037197044@qq.com>
2021-10-26 11:04:29 +0800

    项目完成

--- vue_shop/src/components/goods/Add.vue
+++ vue_shop/src/components/goods/Add.vue
@@ -183,6 +183,7 @@ export default {
       onlyTableData: [],
       // 上传图片的URL地址
       uploadURL: "https://lianghj.top:8888/api/private/v1/upload",
+      // uploadURL: "http://127.0.0.1:8888/api/private/v1/upload",
       // 图片上传组件的headers请求头对象
       headerObj: {
         Authorization: window.sessionStorage.getItem("token"),
--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -16,13 +16,25 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+// 导入 NProgress 对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
-//配置请求的根路径
+// 配置请求的根路径
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+// 在request拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
   // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token');
   // 在最后必须 return config
+  return config
+})
+
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
